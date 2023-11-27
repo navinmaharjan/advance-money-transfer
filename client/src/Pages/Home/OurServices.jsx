d43cd9da5ebd467eea41d22da7059d0e0daad7b9
Nabin Maharjan <maharjan.nabin85@gmail.com>
2023-11-27 11:37:19 +0545

    our services componenet done without icons

--- client/src/Pages/Home/OurServices.jsx
+++ client/src/Pages/Home/OurServices.jsx
@@ -9,11 +9,26 @@ const OurServices = () => {
             Our <span className="text-brand">Services</span>
           </h1>
           <div className="flex gap-4 items-center">
-            <div className="bg-white w-1/3 border p-8 rounded-lg flex flex-col">
+            <div className="bg-white w-1/3 border p-8 rounded-lg h-60 flex flex-col">
               <h1 className="text-2xl text-brand">Immediate Cash</h1>
-              <p className="text-extrathin">
+              <p className="text-xl font-light text-slate-700 pt-4">
                 Immediate cash payment to the "receivers" from convenient
                 location/ paying counters.
+              </p>
+            </div>
+
+            <div className="bg-white w-1/3 border p-8 rounded-lg h-60 flex flex-col">
+              <h1 className="text-2xl text-brand">Deposits</h1>
+              <p className="text-xl font-light text-slate-700 pt-4">
+                Arrange to deposits -total and/or portion of the "send money" as
+                instructed by the "sender" in bank account.
+              </p>
+            </div>
+
+            <div className="bg-white w-1/3 border p-8 rounded-lg h-60 flex flex-col">
+              <h1 className="text-2xl text-brand">Disable Customers</h1>
+              <p className="text-xl font-light text-slate-700 pt-4">
+                Home payment to elderly, sick and disable receivers.
               </p>
             </div>
           </div>
